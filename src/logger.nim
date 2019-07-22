7711d861ff8f8bbedf006e5e5e51431648df221d
Carl Pearson <pearson@illinois.edu>
2019-07-22 18:02:13 -0500

    no debug statements in release mode

--- src/logger.nim
+++ src/logger.nim
@@ -4,7 +4,23 @@ var consoleLog = newConsoleLogger(useStdErr = true)
 
 addHandler(consoleLog)
 
-proc setLevel *(lvl: Level): int {.discardable.} =
+proc setLevel *(lvl: Level) =
     consoleLog.levelThreshold = lvl
 
-export logging
+template debug*(args: varargs[string, `$`]) =
+    while not defined(release):
+        log(lvlDebug, args)
+
+template info*(args: varargs[string, `$`]) =
+    logging.info(args)
+
+template notice*(args: varargs[string, `$`]) =
+    logging.notice(args)
+
+template warn*(args: varargs[string, `$`]) =
+    logging.warn(args)
+
+template error*(args: varargs[string, `$`]) =
+    logging.error(args)
+
+export Level
