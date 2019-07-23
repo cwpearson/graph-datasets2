c187c415c8f63a707f6b57119e29712d1c8c6d13
Carl Pearson <pearson@illinois.edu>
2019-07-23 17:28:25 -0500

    add support for all EdgeStreams

--- src/cmds/tc.nim
+++ src/cmds/tc.nim
@@ -19,7 +19,8 @@ proc toSeconds(d: Duration): float =
 
 
 proc tc (path: string, filter_kind: FilterKind) =
-    let datasetKind = guessFormat(path)
+    notice("open ", path)
+    let es = guessEdgeStreamReader(path)
 
     proc lt(edge: Edge): bool {.inline.} =
         return edge.src > edge.dst
@@ -31,72 +32,68 @@ proc tc (path: string, filter_kind: FilterKind) =
     of fkLower: lt
     of fkUpper: ut
     of fkNone: full
-    case datasetKind
-    of dkBel:
 
+
+    var
+        start: Time
+        elapsed: Duration
+        eps: float
+
+
+
+
+    notice("build CSR")
+    start = getTime()
+    var csr = initCsr[int, int]()
+    for edge in es.edges():
+        if filter(edge):
+            csr.addEdge(edge)
+    csr.finishEdges()
+    elapsed = getTime() - start
+    eps = float(nnz(csr)) / elapsed.toSeconds
+    notice(&"nnz:      {nnz csr}")
+    notice(&"num rows: {numRows csr}")
+    notice(&"{elapsed}, {eps:e}")
+
+    notice("count triangles")
+    start = getTime()
+    var count = 0
+    for edge in csr.edges():
+        let
+            src = edge.src
+            dst = edge.dst
+            srcRow = csr.row(src)
+            dstRow = csr.row(dst)
         var
-            start: Time
-            elapsed: Duration
-            eps: float
-
-        debug("open ", path)
-        let bel = openBelStream(path, fmRead)
-        defer: bel.close()
-
-        notice("build CSR")
-        start = getTime()
-        var csr = initCsr[int, int]()
-        for edge in bel.edges():
-            if filter(edge):
-                csr.addEdge(edge)
-        csr.finishEdges()
-        elapsed = getTime() - start
-        eps = float(nnz(csr)) / elapsed.toSeconds
-        info(&"nnz:      {nnz csr}")
-        info(&"num rows: {numRows csr}")
-        info(&"{elapsed}, {eps:e}")
-
-        notice("count triangles")
-        start = getTime()
-        var count = 0
-        for edge in csr.edges():
-            let
-                src = edge.src
-                dst = edge.dst
-                srcRow = csr.row(src)
-                dstRow = csr.row(dst)
-            var
-                ai = 0
-                bi = 0
+            ai = 0
+            bi = 0
+            loadA = true
+            loadB = true
+            a, b: typeof(srcRow[0])
+        while ai < len(srcRow) and bi < len(dstRow):
+            if loadA:
+                a = srcRow[ai]
+                loadA = false
+            if loadB:
+                b = dstRow[bi]
+                loadB = false
+            if a == b:
+                count += 1
+                ai += 1
+                bi += 1
                 loadA = true
                 loadB = true
-                a, b: typeof(srcRow[0])
-            while ai < len(srcRow) and bi < len(dstRow):
-                if loadA:
-                    a = srcRow[ai]
-                    loadA = false
-                if loadB:
-                    b = dstRow[bi]
-                    loadB = false
-                if a == b:
-                    count += 1
-                    ai += 1
-                    bi += 1
-                    loadA = true
-                    loadB = true
-                elif a < b:
-                    ai += 1
-                    loadA = true
-                else:
-                    bi += 1
-                    loadB = true
-        elapsed = getTime() - start
-        eps = float(nnz(csr)) / elapsed.toSeconds
-        info(&"{elapsed}, {eps:e}")
-        echo count
-    else:
-        error("ERROR can't count for ", path, " of kind ", datasetKind)
-        quit(1)
+            elif a < b:
+                ai += 1
+                loadA = true
+            else:
+                bi += 1
+                loadB = true
+    elapsed = getTime() - start
+    eps = float(nnz(csr)) / elapsed.toSeconds
+    notice(&"{elapsed}, {eps:e}")
+    echo count
+
 
 proc doTc *[T](opts: T): int {.discardable.} =
 
