62d03137603c630df5da955aeb132d89853dd87c
Carl Pearson <pearson@illinois.edu>
2019-07-23 16:37:39 -0500

    add items proc

--- src/edge_stream.nim
+++ src/edge_stream.nim
@@ -6,23 +6,29 @@ type
     EdgeStream* = ref object of RootObj
         stream*: Stream
 
-method readEdge* (s: EdgeStream, edge: var Edge): bool {.base.} = 
+method readEdge*(s: EdgeStream, edge: var Edge): bool {.base.} =
     quit "to be overridden"
 
-method writeEdge* (s: EdgeStream, edge: Edge) {.base.}=
+method writeEdge*(s: EdgeStream, edge: Edge) {.base.} =
     quit "to be overridden"
 
-method atEnd* (s: EdgeStream): bool {.base.} = 
+method atEnd*(s: EdgeStream): bool {.base.} =
     result = s.stream.atEnd()
 
-method getPosition* (s: EdgeStream): int {.base.} = 
-    result = s.stream.getPosition() 
+method getPosition*(s: EdgeStream): int {.base.} =
+    result = s.stream.getPosition()
 
-method setPosition* (s: EdgeStream, pos: int) {.base.} = 
-    s.stream.setPosition(pos) 
+method setPosition*(s: EdgeStream, pos: int) {.base.} =
+    s.stream.setPosition(pos)
 
-method close* (s: EdgeStream) {.base.} = 
+method close*(s: EdgeStream) {.base.} =
     s.stream.close()
+
+iterator items*(s: EdgeStream): Edge =
+    s.setPosition(0)
+    var edge: Edge
+    while s.readEdge(edge):
+        yield edge
 
 iterator edges*(s: EdgeStream): Edge =
     s.setPosition(0)
