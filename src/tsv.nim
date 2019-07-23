5751c96e8249f19190ad8888593f5b25b6f1cc33
Carl Pearson <pearson@illinois.edu>
2019-07-23 15:24:47 -0500

    .

--- src/tsv.nim
+++ src/tsv.nim
@@ -8,19 +8,18 @@ type TsvStream* = ref object of EdgeStream
     line: string
 
 method readEdge *(s: TsvStream, edge: var Edge): bool =
-    let good = s.stream.readLine(s.line)
-    if good:
+    result = s.stream.readLine(s.line)
+    if result:
         let fields: seq[string] = s.line.splitWhitespace()
         edge.src = parseInt fields[0]
         edge.dst = parseInt fields[1]
         edge.weight = parseFloat fields[2]
-    good
 
 method writeEdge *(s: TsvStream, edge: Edge) =
     let str = $edge.src & "\t" & $edge.dst & "\t" & $int(edge.weight)
     s.stream.writeLine(str)
 
-proc newTsvStream *(stream: Stream): TsvStream = 
+proc newTsvStream *(stream: Stream): TsvStream =
     new(result)
     result.stream = stream
 
