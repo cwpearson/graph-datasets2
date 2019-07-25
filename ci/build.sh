00106d8c0dc284f5d6ad72c34f4fd398f4bedb97
Carl Pearson <carl.w.pearson@gmail.com>
2019-07-25 06:34:23 -0500

    do a release build on travis

--- ci/build.sh
+++ ci/build.sh
@@ -2,11 +2,11 @@ set -eu
 set -x
 
 if [ -n "${CROSS_CPU+x}" ]; then
-    nimble build --cpu:$CROSS_CPU --os:linux
+    nimble build --cpu:$CROSS_CPU --os:linux -d:release
 elif [ -n "${CROSS_OS+x}" ]; then
     if [ "$CROSS_OS" = "windows" ]; then
-        nimble build -d:mingw --cpu:amd64
+        nimble build -d:mingw --cpu:amd64 -d:release
     fi
 else
-    nimble build
+    nimble build -d:release
 fi
