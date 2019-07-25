5b801c7031963b1e8fcc3cc68231d1cb69dcb1b1
Carl Pearson <carl.w.pearson@gmail.com>
2019-07-25 07:02:35 -0500

    add .exe to windows binary

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -6,7 +6,11 @@ mkdir -p deploy
 if [ -n "${CROSS_CPU+x}" ]; then
     mv graph_datasets deploy/graph_datasets-linux-$CROSS_CPU
 elif [ -n "${CROSS_OS+x}" ]; then
-    mv graph_datasets deploy/graph_datasets-$CROSS_OS-amd64
+    if [ "$CROSS_OS" = "windows" ]; then
+        mv graph_datasets deploy/graph_datasets-$CROSS_OS-amd64.exe
+    else
+        mv graph_datasets deploy/graph_datasets-$CROSS_OS-amd64
+    fi
 elif [ "${CHANNEL}" = "stable" ]; then
     mv graph_datasets deploy/graph_datasets-$TRAVIS_OS_NAME-amd64
 fi
