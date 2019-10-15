759db0fd74bb9822c939392727199c089200952c
Carl Pearson <pearson@illinois.edu>
2019-10-15 17:09:27 -0500

    add gd_count to release

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -4,14 +4,15 @@ set -x
 mkdir -p deploy
 
 if [ -n "${CROSS_CPU+x}" ]; then
-    zip -r deploy/graph_datasets-linux-$CROSS_CPU.zip graph_datasets
+    zip -r deploy/graph_datasets-linux-$CROSS_CPU graph_datasets gd_count
 elif [ -n "${CROSS_OS+x}" ]; then
     if [ "$CROSS_OS" = "windows" ]; then
         mv graph_datasets graph_datasets.exe
-        zip -r deploy/graph_datasets-$CROSS_OS-amd64 graph_datasets.exe
+        mv gd_count gd_count.exe
+        zip -r deploy/graph_datasets-$CROSS_OS-amd64 graph_datasets.exe gd_count.exe
     else
-        zip -r deploy/graph_datasets-$CROSS_OS-amd64 graph_datasets
+        zip -r deploy/graph_datasets-$CROSS_OS-amd64 graph_datasets gd_count
     fi
 elif [ "${CHANNEL}" = "stable" ]; then
-    zip -r deploy/graph_datasets-$TRAVIS_OS_NAME-amd64 graph_datasets
+    zip -r deploy/graph_datasets-$TRAVIS_OS_NAME-amd64 graph_datasets gd_count
 fi
